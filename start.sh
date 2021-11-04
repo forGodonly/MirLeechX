c9574e1c925718e441bf2f5fb97b58922154dae0
Elias Benbourenane <eliasbenbourenane@gmail.com>
2021-11-04 11:11:33 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,13 @@ qbit_trackers_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersLis
 echo -e "\nmax-concurrent-downloads=7\nbt-tracker=$tracker_list" >> /usr/src/app/aria.conf
 echo -e "\nBittorrent\add_trackers=$=$qbit_trackers_list" >> /usr/src/app/qBittorrent.conf
 aria2c --conf-path=/usr/src/app/aria.conf
+
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+cp rclone /usr/bin/
+chown root:root /usr/bin/rclone
+chmod 755 /usr/bin/rclone
+cd ..
+
 python3 -m bot
